e68bf58a51605dba2c2d913cdd007155e218c19b
jorgefortunatof <jorgefortunatof@gmail.com>
2021-04-18 19:00:30 -0300

    style adjustments and added weighted average on greyscale

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-const canvas = document.getElementById("frame");
-const ctx = canvas.getContext('2d');
+const canvasResult = document.getElementById("frame3");
+const ctxResult = canvasResult.getContext('2d');
+
+const canvas1 = document.getElementById("frame1");
+const ctx1 = canvas1.getContext('2d');
 
 window.onload = () => {
 	document.getElementById('grayscale').onclick = grayscale;
-	document.getElementById('image').onchange = onUpdateImage;
+	document.getElementById('image1').onchange = onUpdateImage;
 
-	canvas.onmousemove = mousePicker;
+	canvas1.onmousemove = mousePicker;
 }
 
 function mousePicker(event){
 	const x = event.layerX;
   const y = event.layerY;
 
-	const pixel = ctx.getImageData(x, y, 1, 1);
+	const pixel = ctx1.getImageData(x, y, 1, 1);
 	const data = pixel.data;
 
 	const pixelR = data[0]
@@ -50,10 +53,10 @@ function drawImage(url) {
 	let image = new Image();
 
 	image.onload = () => {
-		canvas.width = image.width;
-		canvas.height = image.height;
+		canvas1.width = image.width;
+		canvas1.height = image.height;
 
-		ctx.drawImage(image, 0, 0, image.width, image.height);
+		ctx1.drawImage(image, 0, 0, image.width, image.height);
 	}
 	
 	image.src = url;	
@@ -62,15 +65,39 @@ function drawImage(url) {
 
 /* PDI */
 function grayscale(){
-	const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+	const isWeightedAverage = document.getElementById('grayscale-type').checked;
+	const redPercent = Number(document.getElementById('r%').value);
+	const greenPercent = Number(document.getElementById('g%').value);
+	const bluePercent = Number(document.getElementById('b%').value);
+
+	const imageData = ctx1.getImageData(0, 0, canvas1.width, canvas1.height);
 	const data = imageData.data;
 
-	for (var i = 0; i < data.length; i += 4) {
-			var avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
-			data[i]     = avg; // red
-			data[i + 1] = avg; // green
-			data[i + 2] = avg; // blue
+	if(isWeightedAverage){
+		if(redPercent + greenPercent + bluePercent !== 100){
+			alert('A soma das porcentagens deve ser igual à 100!');
+			return;
+		}
+
+		for (let i = 0; i < data.length; i += 4) {
+				let avg = ((data[i] * redPercent) + (data[i + 1] * greenPercent) + (data[i + 2] * bluePercent)) 
+				/ (redPercent + greenPercent + bluePercent);
+
+				data[i]     = avg; // red
+				data[i + 1] = avg; // green
+				data[i + 2] = avg; // blue
+		}
+	}else{
+		for (let i = 0; i < data.length; i += 4) {
+				let avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+				data[i]     = avg; // red
+				data[i + 1] = avg; // green
+				data[i + 2] = avg; // blue
+		}
 	}
-	
-	ctx.putImageData(imageData, 0, 0);
+
+	canvasResult.width = canvas1.width;
+	canvasResult.height = canvas1.height;
+
+	ctxResult.putImageData(imageData, 0, 0);
 }
